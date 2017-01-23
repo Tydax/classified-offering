d37d751334e374512622a74aeb648e01caf850e6
Armand (Tydax) Bour <armand.bour@etudiant.univ-lille1.fr>
2017-01-23 15:00:17 +0100

    Initialise repository with packages
    Why? Because apparently, NuGet is so good that it cannot restore my
    packages. Awesome, huh?

---
+++ classified-offering/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(classified_offering.Startup))]
+namespace classified_offering
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
